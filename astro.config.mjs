b629641562df686039df6f0485407c6466bf652c
Greg Signal <glsignal@users.noreply.github.com>
2024-12-20 09:53:28 +0100

    chore(deps): bulk updates (#129)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,21 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
+import icon from "astro-icon";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alasco.tech",
+  output: "static",
+  outDir: "./dist",
   integrations: [
     mdx(),
     sitemap(),
     tailwind(),
     prefetch(),
+    icon(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
